1f97b6926555257d26ca7e2fe6f5b963755b5992
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-02-11 16:12:29 -0300

    Adding Cross origin annotation

--- src/main/java/com/br/felipe/ecommerce/controllers/ItemController.java
+++ src/main/java/com/br/felipe/ecommerce/controllers/ItemController.java
@@ -9,6 +9,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class ItemController {
 	@Autowired
 	ItemRepository repo;
 	
-	
+	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping("/items")
 	public ResponseEntity<List<Item>> getAllItems(){
 		List<Item> itemlist = repo.findAll();
