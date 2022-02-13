717b3141d1a00a44d3cfa09a43e0632d1ee9ede4
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-02-13 17:44:27 -0300

    Finally fixing the 500 error.

--- src/main/java/com/br/felipe/ecommerce/EcommerceApplication.java
+++ src/main/java/com/br/felipe/ecommerce/EcommerceApplication.java
@@ -1,13 +1,17 @@
 package com.br.felipe.ecommerce;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class EcommerceApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
+	
 
 }
